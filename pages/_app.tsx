5133a24ab6d0e34734ffb15d82d04f2ae9394df7
Minchan <98478661+itscold96@users.noreply.github.com>
2024-09-17 14:19:41 +0900

    #21 fix: 푸터가 랜딩 페이지와 메인 페이지에만 존재하도록 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <Providers>
         <Header />
         <Component {...pageProps} />
-        <Footer />
       </Providers>
     </>
   );
